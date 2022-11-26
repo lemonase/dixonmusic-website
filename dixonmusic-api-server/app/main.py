a6ed32b8ef4113b24f10398e5f1449fe1d223008
James Dixon <notjamesdixon@gmail.com>
2022-11-26 14:23:23 -0500

    move all code to main.py and create Dockerfile

--- dixonmusic-api-server/app/main.py
+++ dixonmusic-api-server/app/main.py
@@ -1,19 +1,19 @@
-# import discogs_client
+from typing import Union
 import json
-from pprint import pprint
 import os
 
+from fastapi import FastAPI
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-def get_inventory(userName="dixonmusic",
-                  apiToken=os.environ.get("DIXONMUSIC_API_TOKEN"),
-                  page=1,
-                  per_page=100
-                  ):
+def fetch_inventory(userName="dixonmusic",
+                    apiToken=os.environ.get("DIXONMUSIC_API_TOKEN"),
+                    page=1,
+                    per_page=100
+                    ):
     api_url = f"https://api.discogs.com/users/{userName}/inventory"
 
     query_params = {
@@ -34,3 +34,18 @@ def get_primary_image(images):
             return image
 
     return images[0]
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/inventory/")
+async def listings(page: Union[int, None] = 1,
+                   per_page: Union[int, None] = 100,
+                   ):
+    return fetch_inventory(page=page, per_page=per_page)
