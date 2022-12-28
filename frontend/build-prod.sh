a12706f3c6680b82fbb8f58d195f6f3e246be62d
James Dixon <notjamesdixon@gmail.com>
2022-12-28 19:13:24 +0000

    update build script

--- frontend/build-prod.sh
+++ frontend/build-prod.sh
@@ -1,4 +1,5 @@
 # build prod nginx Dockerfile and copy out the vite build files
 docker image build --file Dockerfile --tag dixonmusic-frontend:prod .
 CID=$(docker run --rm -d dixonmusic-frontend:prod)
+rm -r dist/
 docker cp $CID:/usr/share/nginx/html dist/ && docker stop $CID
