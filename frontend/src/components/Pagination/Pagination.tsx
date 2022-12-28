4e440e7be5d7320e5fdc874864ffa016cf34bf7e
James Dixon <notjamesdixon@gmail.com>
2022-12-28 12:01:50 -0500

    get started on pagination

--- frontend/src/components/Pagination/Pagination.tsx
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { ButtonGroup, Button } from 'react-bootstrap';
 
 const Pagination = (props) => {
+    const [currentPage, setCurrentPage] = useState(props.pagination["page"]);
+    const [lastPage, setLastPage] = useState(props.pagination["pages"]);
+
+    useEffect(() => {
+        return () => {
+
+        }
+    }, [currentPage])
+
+
+    const handlePageButtonClick = (event: React.MouseEvent<HTMLElement>) => {
+        // console.log(event.target.id)
+        setCurrentPage(event.target.id);
+    }
+
+    const PageButtons = () => {
+        let btns = [];
+        for (let i = 0; i < lastPage; i++) {
+            btns.push(<Button key={i} id={"button-" + i.toString()} className="page-button" onClick={handlePageButtonClick} > {i + 1}</Button >)
+        }
+        return btns;
+    }
+
     return (
         <>
             <div>Pagination</div>
@@ -10,6 +35,13 @@ const Pagination = (props) => {
             <li> Page: {props.pagination["page"]} </li>
             <li> All Pages: {props.pagination["pages"]} </li>
             <li> Per Pages: {props.pagination["per_page"]} </li>
+            <li> Total Items: {props.pagination["items"]}</li>
+            <li> Next Page: {props.pagination.urls["next"]}</li>
+            <li> Last Page: {props.pagination.urls["last"]}</li>
+
+            <ButtonGroup>
+                <PageButtons></PageButtons>
+            </ButtonGroup>
         </>
     )
 }
