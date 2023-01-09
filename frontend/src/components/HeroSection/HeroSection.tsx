0b9ec42beaa2c39a27c8dc041878fa8b0362b7f6
James Dixon <notjamesdixon@gmail.com>
2023-01-08 22:05:15 -0500

    drop the s on Vinyl

--- frontend/src/components/HeroSection/HeroSection.tsx
+++ frontend/src/components/HeroSection/HeroSection.tsx
@@ -6,7 +6,7 @@ function HeroSection() {
   return (
     <>
       <div className="hero-heading-section">
-        <h2>Looking for Records, Vinyls, CDs?</h2>
+        <h2>Looking for Records, Vinyl, CDs?</h2>
       </div>
     </>
   );
